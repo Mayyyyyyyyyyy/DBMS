b7f2053231abf51a9038e5358b41751cbf8c5046
Mayyyyyyyyyyy <93366383+Mayyyyyyyyyyy@users.noreply.github.com>
2023-05-24 21:12:13 +0800

    資料庫連起來了啊啊啊啊啊啊

--- DBMS_21/src/main/java/Server.java
+++ DBMS_21/src/main/java/Server.java
@@ -1,9 +1,12 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.protobuf.TextFormat.Printer;
 
 @WebServlet("/Server")
@@ -20,14 +24,34 @@ public class Server extends HttpServlet {
 	private String account;
 	private String password;
 
-	public Server() {
-		super();
-		SetUp setUp = new SetUp();
-		conn = setUp.getConn();
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(
+				"C:/Users/user/eclipse-workspace/DBMS_21/organic-phoenix-387005-45309f4d7fba.json"));
+
+		// 建立資料庫連線
+		String instanceConnectionName = "organic-phoenix-387005:asia-east1:ryuuyo39";
+		String databaseName = "jobsearchplatform";
+//        String jdbcUrl = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s&socketFactory=%s",
+//                databaseName, instanceConnectionName, CoreSocketFactory.class.getName());
+		String jdbcUrl = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s&"
+				+ "socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8",
+				databaseName, instanceConnectionName);
+
+		Properties properties = new Properties();
+		properties.setProperty("user", "root");
+		properties.setProperty("password", "Umi8491lulu");
+
+		properties.setProperty("credentials", credentials.toString());
+
+		try {
+			conn = DriverManager.getConnection(jdbcUrl, properties);
+			System.out.println(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
@@ -39,11 +63,10 @@ public class Server extends HttpServlet {
 			request.setAttribute("requestUri", requestUri);
 			request.getRequestDispatcher("Login.jsp").forward(request, response);
 			System.out.println("vvvvvvvvvvvv");
-			System.out.println(conn);
 			return;
 		}
 
-		System.out.println("rrrrrrrrrrrrrrrr");
+		
 		account = request.getParameter("loginAccount");
 		password = request.getParameter("loginPassword");
 		System.out.println(account + " " + password);
@@ -54,8 +77,6 @@ public class Server extends HttpServlet {
 
 			stat.setString(1, account);
 			stat.setString(2, password);
-			stat.executeQuery();
-			stat.clearParameters();
 
 			ResultSet resultSet = stat.executeQuery();
 			System.out.println("xxxxxxxxxxxxxxxxxxx");
@@ -68,6 +89,8 @@ public class Server extends HttpServlet {
 					request.getRequestDispatcher("Homepage.jsp").forward(request, response);
 				}
 			}
+
+			stat.clearParameters();
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
